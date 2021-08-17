b258de8d327b2819ef51017c17b4a0957d70a5dd
rani1029 <ranig1029@gmail.com>
2021-08-17 17:31:45 +0530

    welcome msg added file added

---
+++ SnakeNLadder2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeNLadder2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to snake and ladder simulation");
+        }
+    }
+}
