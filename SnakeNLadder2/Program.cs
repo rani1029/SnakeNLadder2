2c4589c9f79f42d9e25afad21fd34f6a332cbcf3
rani1029 <ranig1029@gmail.com>
2021-08-18 00:18:43 +0530

    use case 3 file added

--- SnakeNLadder2/Program.cs
+++ SnakeNLadder2/Program.cs
@@ -5,20 +5,49 @@ namespace SnakeNLadder2
     class Program
     {
 
-        //int playerPosition = 0;
         static void Main(string[] args)
         {
+            Console.WriteLine("welcome to snake and ladder simulation");
             int playerPosition = 0;
             int diceOutput = 0;
+            int diceRoll = 0;
+            while (playerPosition != 100)
+            {
+                Random rm = new Random();
+                diceOutput = rm.Next(1, 7);
+                Console.WriteLine("diceOutput is " + diceOutput);
+                Random rm2 = new Random();
+                int playerMove = rm2.Next(1, 4);
+                switch (playerMove)
+                {
+                    case 1:
+                        Console.WriteLine("no playermove");
+                        Console.WriteLine("playerPosition is " + playerPosition);
+                        break;
+                    case 2:
+                        Console.WriteLine(" playermove is ladder");
+                        playerPosition += diceOutput;
+                        Console.WriteLine("playerPosition is " + playerPosition);
+                        if (playerPosition > 100)
+                        {
+                            playerPosition += 0;
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("playermove is snake");
+                        playerPosition -= diceOutput;
+                        Console.WriteLine("playerPosition is " + playerPosition);
+                        if (playerPosition < 0)
+                        {
+                            playerPosition = 0;
+                        }
+                        break;
 
 
-            Console.WriteLine("welcome to snake and ladder simulation");
-            Random rm = new Random();
-            diceOutput = rm.Next(1, 7);
-            Console.WriteLine("diceOutput is " + diceOutput);
-            //Random rm2 = new Random();
-            //int playerMove = rm2.Next(1, 7);
+                }
+            }
         }
     }
-
 }
+
+
