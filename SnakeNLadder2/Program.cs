2f3646b4da5f1ad5d5ae3c2dac8501d6f48ffc90
rani1029 <ranig1029@gmail.com>
2021-08-17 17:48:02 +0530

    use case1  added file added

--- SnakeNLadder2/Program.cs
+++ SnakeNLadder2/Program.cs
@@ -4,9 +4,14 @@ namespace SnakeNLadder2
 {
     class Program
     {
+
+        int playerPosition = 0;
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to snake and ladder simulation");
+            Random rm2 = new Random();
+            int playerMove = rm2.Next(1, 7);
         }
     }
+
 }
