d90655426bbc3c3cd85a6a99871a75cb953057d6
rani1029 <ranig1029@gmail.com>
2021-08-18 00:04:30 +0530

    use case 2 file added

--- SnakeNLadder2/Program.cs
+++ SnakeNLadder2/Program.cs
@@ -5,12 +5,19 @@ namespace SnakeNLadder2
     class Program
     {
 
-        int playerPosition = 0;
+        //int playerPosition = 0;
         static void Main(string[] args)
         {
+            int playerPosition = 0;
+            int diceOutput = 0;
+
+
             Console.WriteLine("welcome to snake and ladder simulation");
-            Random rm2 = new Random();
-            int playerMove = rm2.Next(1, 7);
+            Random rm = new Random();
+            diceOutput = rm.Next(1, 7);
+            Console.WriteLine("diceOutput is " + diceOutput);
+            //Random rm2 = new Random();
+            //int playerMove = rm2.Next(1, 7);
         }
     }
 
